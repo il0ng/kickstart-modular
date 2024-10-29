3794fa18c5f3187eec5a557727b9d77473d07cf7
omatica <ian@omatica>
2024-10-29 21:15:36 +0000

    switched icon set for oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -190,6 +190,6 @@ return {
     },
   },
   -- Optional dependencies
-  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
 }
