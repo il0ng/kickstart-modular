9399f3daba76c51e8f9ebc848aeb50af61bf09b8
omatica <ian@omatica>
2024-11-12 22:34:41 +0000

    added neogit telescope keymaps

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -10,8 +10,8 @@ return {
   config = function()
     vim.api.nvim_set_keymap('n', '<leader>g', ':Neogit kind=replace<CR>', { noremap = true, silent = true, desc = 'Open Neogit' })
     vim.api.nvim_set_keymap('n', '<leader>gs', ':Neogit kind=split<CR>', { noremap = true, silent = true, desc = 'Open Neogit in split' })
-    vim.api.nvim_set_keymap('n', '<leader>gb', ':Telescope git_branches<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gc', ':Telescope git_commits<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gb', ':Telescope git_branches<CR>', { noremap = true, silent = true, desc = 'Search branches' })
+    vim.api.nvim_set_keymap('n', '<leader>gc', ':Telescope git_commits<CR>', { noremap = true, silent = true, desc = 'Search commits' })
 
     require('neogit').setup {
       integrations = {
