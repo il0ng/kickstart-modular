3b0e035096c10126a955911f78de224fb3eaba38
omatica <ian@omatica>
2024-11-12 21:30:13 +0000

    updated neogit config

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -1,17 +1,24 @@
-vim.api.nvim_set_keymap('n', '<leader>g', ':Neogit<CR>', { noremap = true, silent = true, desc = 'Open Neogit' })
-vim.api.nvim_set_keymap('n', '<leader>gs', ':Neogit kind=split<CR>', { noremap = true, silent = true, desc = 'Open Neogit in split' })
--- vim.api.nvim_set_keymap('n', '<leader>ngc', ':Neogit commit<CR>', { noremap = true, silent = true, desc = 'Open Neogit commit' })
-
 return {
   'NeogitOrg/neogit',
-  dependencies = {
-    'nvim-lua/plenary.nvim', -- required
-    'sindrets/diffview.nvim', -- optional - Diff integration
 
-    -- Only one of these is needed.
-    'nvim-telescope/telescope.nvim', -- optional
-    'ibhagwan/fzf-lua', -- optional
-    'echasnovski/mini.pick', -- optional
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'sindrets/diffview.nvim',
+    'nvim-telescope/telescope.nvim',
   },
-  config = true,
+
+  config = function()
+    vim.api.nvim_set_keymap('n', '<leader>g', ':Neogit kind=replace<CR>', { noremap = true, silent = true, desc = 'Open Neogit' })
+    vim.api.nvim_set_keymap('n', '<leader>gs', ':Neogit kind=split<CR>', { noremap = true, silent = true, desc = 'Open Neogit in split' })
+    vim.api.nvim_set_keymap('n', '<leader>gb', ':Telescope git_branches<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gc', ':Telescope git_commits<CR>', { noremap = true, silent = true })
+
+    require('neogit').setup {
+      integrations = {
+        diffview = true,
+        telescope = true,
+      },
+      -- graph_style = 'kitty',
+    }
+  end,
 }
