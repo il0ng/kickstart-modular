19e3b3a1648fd9a89ed1bcf3ba39e580642e3fc4
omatica <ian@omatica>
2024-11-21 06:51:08 +0000

    plugin config updates. fzf-lua added

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -4,19 +4,21 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'sindrets/diffview.nvim',
-    'nvim-telescope/telescope.nvim',
+    -- 'nvim-telescope/telescope.nvim',
+    'ibhagwan/fzf-lua',
   },
 
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>g', ':Neogit kind=replace<CR>', { noremap = true, silent = true, desc = 'Open Neogit' })
-    vim.api.nvim_set_keymap('n', '<leader>gs', ':Neogit kind=split<CR>', { noremap = true, silent = true, desc = 'Open Neogit in split' })
-    vim.api.nvim_set_keymap('n', '<leader>gb', ':Telescope git_branches<CR>', { noremap = true, silent = true, desc = 'Search branches' })
-    vim.api.nvim_set_keymap('n', '<leader>gc', ':Telescope git_commits<CR>', { noremap = true, silent = true, desc = 'Search commits' })
+    vim.keymap.set('n', '<leader>g', ':Neogit kind=replace<CR>', { noremap = true, silent = true, desc = 'Open Neogit' })
+    vim.keymap.set('n', '<leader>gs', ':Neogit kind=split<CR>', { noremap = true, silent = true, desc = 'Open Neogit in split' })
+    vim.keymap.set('n', '<leader>gb', ':FzfLua git_branches<CR>', { noremap = true, silent = true, desc = 'Search branches' })
+    vim.keymap.set('n', '<leader>gc', ':FzfLua git_commits<CR>', { noremap = true, silent = true, desc = 'Search commits' })
 
     require('neogit').setup {
       integrations = {
         diffview = true,
-        telescope = true,
+        -- telescope = true,
+        fzf_lua = true,
       },
       graph_style = 'kitty',
     }
